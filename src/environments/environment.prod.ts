dad4053992d3d446d1ba326c7274dc47c3458946
Miguel Miranda Colis <44578094+CrowliDz@users.noreply.github.com>
2022-02-23 20:52:38 -0600

    cc

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  urlEndPoint: 'http://3.133.115.135:5000'
+  urlEndPoint: 'http://3.16.1.247:5000'
   //urlEndPoint: 'http://localhost:5000'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  urlEndPoint: 'http://3.133.115.135:5000' //
+  urlEndPoint: 'http://3.16.1.247:5000' //
   //urlEndPoint: 'http://localhost:5000' //
 };
 
